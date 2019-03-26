d717e4cd263f912877e5e402dbdcc42ac9dbc884
Brendan Hellar and Brendan Moroso <brendan@>
2019-03-26 16:40:08 -0700

    sucessful toggle function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+var flag = true
+var dogflag = true
+
 $(document).ready(function(){
   $("button#cat").click(function(){
     $("p.cattalk").prepend("<p>Meow, hater</p>");
@@ -5,12 +8,23 @@ $(document).ready(function(){
   $("button#dog").click(function(){
     $("p.dogtalk").prepend("<p>Ruff ruff, hater</p>");
   });
-  $("img#catjpg").click(function(){
-    $("img#catjpg").attr('src', "images/cat2.jpg");
+  $("img#catjpg").click(function() {
+    if (flag){
+      $(this).attr("src", "images/cat2.jpg");
+      flag = false;
+    }else {
+        $(this).attr("src", "images/cat.jpg");
+        flag = true;
+    }
   });
   $("img#dogjpg").click(function(){
-    $("img#dogjpg").attr('src', "images/dog2.jpg");
+  if (dogflag) {
+    $(this).attr("src", "images/dog2.jpg");
+    dogflag = false;
+
+  }else{
+    $(this).attr("src", "images/dog.jpg");
+    dogflag = true;
+  }
   });
-  $('src', "images/dog2.jpg").click(function(){
-    $("img#dogjpg").attr('src', "images/dog.jpg");{
 });
